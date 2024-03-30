a0cba3f2934761e719d569e414e00596dd2c64d9
madhan4187 <massmadhan25@gmail.com>
2024-03-30 07:51:34 +0000

    DB error

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path')
+
+const {open} = require('sqlite')
+const sqlite3 = require('sqlite3')
+const app = express()
+
+const dbPath = path.join(__dirname, 'cricketTeam.db')
+let db = null
+const initiziseDbAndServer = async () => {
+  try {
+    db = await open({
+      fileName: dbPath,
+      driver: sqlite3.DataBase,
+    })
+    app.listen(3000, () => {
+      console.log('Server is running at http://localhost:3000/')
+    })
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`)
+    process.exit(1)
+  }
+}
+initiziseDbAndServer()
